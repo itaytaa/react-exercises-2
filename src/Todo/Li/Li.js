2d862436a88aade1bbf8f02720b4b9cdf263f8c9
itay taasa <itaytaasa@gmail.com>
2021-01-15 15:24:36 +0200

    done

---
+++ src/Todo/Li/Li.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Li(props) {
+
+    const task=props.task
+    const remove= props.remove
+
+    return (
+        <li>
+            {task} <button onClick={remove} >X</button>
+        </li>
+    )
+}
+
+export default Li
--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './Todo.scss';
+import Li from './Li/Li';
 
 function Todo() {
+
+	const input = useRef()
+	const [tasks, setTasks] = useState([]);
+
+	function submit(e) {
+		e.preventDefault();
+		setTasks([...tasks, input.current.value])
+		e.target.reset()
+
+
+	}
+	function remove(idx) {
+		let copy = tasks.filter((task, index) =>  index !== idx )
+			setTasks(copy)
+	}
+
 	return (
-		<div className="Todo">
-			<h3>Todo List:</h3>
-			<p>Make a Todo list (add & delete options):</p>
-			<input placeholder="Your task..." /> <button>Add task</button>
+		<div className="Todo" >
+			<form onSubmit={(e) => submit(e)}>
+				<h3>Todo List:</h3>
+				<p>Make a Todo list (add & delete options):</p>
+				<input placeholder="Your task..." ref={input} /> <button>Add task</button>
+			</form>
+			<ul>
+				{tasks.map((task, index) => {
+
+					return <Li task={task} key={index} remove={() => remove(index)} />
+				})}
+
+			</ul>
+
 		</div>
 	)
 }
